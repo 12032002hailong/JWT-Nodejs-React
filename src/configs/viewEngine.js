13bab3428779671e414ac890af9149495e0d1278
Hai Long <hailongkc1203@gmail.com>
2022-12-22 13:43:34 +0700

    hello -world

---
+++ src/configs/viewEngine.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+
+/**
+ * 
+ * @param {*} app --express app 
+ */
+const configViewEngine = (app) => {
+    app.use(express.static('./src/public'));
+    app.set("view engine", "ejs");
+    app.set("views", "./src/views")
+
+
+}
+
+
+//exp =  export default 
+export default configViewEngine;
