503cad25b41e023e3cbcb91424134828144187cb
Hai Long <hailongkc1203@gmail.com>
2022-12-22 15:56:56 +0700

    Hash User Password

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,14 +1,4 @@
-import mysql from 'mysql2';
-
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt'
-});
-
-
-
+import userService from '../service/userServices';
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs");
 }
@@ -23,17 +13,13 @@ const handleCreateNewUser = (req, res) => {
     let username = req.body.username;
 
 
-    connection.query(
-        ` INSERT INTO users (email, password, username) VALUES (?, ?, ? )`, [email, password, username],
-        function (err, results, fields) {
-            if (err) {
-                console.log(err);
-            }
 
-        }
-    );
+    // userService.createNewUser(email, password, username);
+    userService.getUserList();
     return res.send("handleCreateNewUser");
 }
+
+
 
 module.exports = {
     handleHelloWorld, handleUserPage,
